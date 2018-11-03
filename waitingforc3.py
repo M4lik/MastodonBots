c4e8c3d75f8ebc8116d3b436e9491bec13587ed9
Till <mail@llit.eu>
2018-11-03 22:08:57 +0100

    removed typo, added comments

--- waitingforc3.py
+++ waitingforc3.py
@@ -3,8 +3,8 @@ import time
 import datetime
 
 mastodon = Mastodon(
-    access_token = 'waitingforc3.secret',
-    api_base_url = 'https://botsin.space'
+    access_token = 'waitingforc3.secret',   # File for "Your access token", can be created unter "Settings" -> "Development" -> "Your applications"
+    api_base_url = 'https://botsin.space'   # URL that the bot is hosted on
 )
 
 _today = datetime.date.today()
@@ -13,20 +13,26 @@ Dez28 = datetime.date(_today.year, 12, 28)
 Dez29 = datetime.date(_today.year, 12, 29)
 Dez30 = datetime.date(_today.year, 12, 30)
 Dez31 = datetime.date(_today.year, 12, 31)
-hashtag = "#" + str(_today.year - 1983) + "C3"
-nextHashtag = "#" + str(_today.year - 1982) + "C3"
+hashtag = "#" + str(_today.year - 1983) + "C3"      # Hasthag of this years C3
+nextHashtag = "#" + str(_today.year - 1982) + "C3"  # Hashtag of next years C3
 
 daysUntiltheDay = (theDay - _today).days
 
 if theDay == _today :
+    # Post for 1st day of C3
     mastodon.status_post("Today is the day! 0 Days until " + hashtag + "! \nHave a very excellent Chaos Communication Congress. \\o/")
 elif Dez28 == _today :
+    # Post for 2nd day of C3
     mastodon.status_post("It's the second day of " + hashtag + ". \nLet's light up all the LEDs and make all the network cables glow!")
 elif Dez29 == _today :
+    # Post for 3rd day of C3
     mastodon.status_post("The third day of " + hashtag + " is in full progress.\nDrink all the mate and don't forget to wash your hands!")
 elif Dez30 == _today :
-    mastodon.status_post("The fourth and final day of the " + hashtag + "has begun. \nLet's party hack hard and party harder!")
+    # Post for 4th and last day of C3
+    mastodon.status_post("The fourth and final day of the " + hashtag + "has begun. \nLet's hack hard and party harder!")
 elif Dez31 == _today :
+    # Post for day after C3
     mastodon.status_post("That was " + hashtag + "! What a blast! \nA good and save way home to everyone. See you at " + nextHashtag + "!")
 else :
+    # Post for every other day
     mastodon.status_post("We are happy to announce that there are only " + str(daysUntiltheDay) + " Days left until " + hashtag + "!")
--- whatthecommit.py
+++ whatthecommit.py
@@ -2,8 +2,8 @@ from mastodon import Mastodon
 import urllib.request
 
 mastodon = Mastodon(
-    access_token = 'whatthecommit.secret',
-    api_base_url = 'https://botsin.space'
+    access_token = 'whatthecommit.secret',  # File for "Your access token", can be created unter "Settings" -> "Development" -> "Your applications"
+    api_base_url = 'https://botsin.space'   # URL that the bot is hosted on
 )
 
 commit = urllib.request.urlopen('https://whatthecommit.com/index.txt').read().decode('utf-8').rstrip()
