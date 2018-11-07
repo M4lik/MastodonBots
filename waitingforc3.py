cd53b159acce76eefffdff8ddf6fe9c7356dccc7
t-aus-m <github@login.llit.eu>
2018-11-07 14:08:10 +0100

    added retrieval of announcments.txt, changed structure of announcements.txt

--- waitingforc3.py
+++ waitingforc3.py
@@ -1,6 +1,7 @@
 from mastodon import Mastodon
 import time
 import datetime
+import urllib.request
 
 mastodon = Mastodon(
     access_token = 'waitingforc3.secret',   # File for "Your access token", can be created unter "Settings" -> "Development" -> "Your applications"
@@ -15,24 +16,24 @@ Dez30 = datetime.date(_today.year, 12, 30)
 Dez31 = datetime.date(_today.year, 12, 31)
 hashtag = "#" + str(_today.year - 1983) + "C3"      # Hasthag of this years C3
 nextHashtag = "#" + str(_today.year - 1982) + "C3"  # Hashtag of next years C3
-
+stringList = urllib.request.urlopen("https://raw.githubusercontent.com/t-aus-m/MastodonBots/testing/announcements.txt").read().decode('utf-8').splitlines()
 daysUntiltheDay = (theDay - _today).days
 
 if theDay == _today :
     # Post for 1st day of C3
-    mastodon.status_post("Today is the day! 0 Days until " + hashtag + "! \nHave a very excellent Chaos Communication Congress. \\o/")
+    print()
 elif Dez28 == _today :
     # Post for 2nd day of C3
-    mastodon.status_post("It's the second day of " + hashtag + ". \nLet's light up all the LEDs and make all the network cables glow!")
+    print()
 elif Dez29 == _today :
     # Post for 3rd day of C3
-    mastodon.status_post("The third day of " + hashtag + " is in full progress.\nDrink all the mate and don't forget to wash your hands!")
+    print()
 elif Dez30 == _today :
     # Post for 4th and last day of C3
-    mastodon.status_post("The fourth and final day of the " + hashtag + "has begun. \nLet's hack hard and party harder!")
+    print()
 elif Dez31 == _today :
     # Post for day after C3
-    mastodon.status_post("That was " + hashtag + "! What a blast! \nA good and save way home to everyone. See you at " + nextHashtag + "!")
+    print()
 else :
     # Post for every other day
-    mastodon.status_post("We are happy to announce that there are only " + str(daysUntiltheDay) + " Days left until " + hashtag + "!")
+    print(stringList)
