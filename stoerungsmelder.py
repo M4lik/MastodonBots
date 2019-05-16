5797a648c43c7d7c9e4e076bce102bd8f99cda84
t-aus-m <t-aus-m@users.noreply.github.com>
2019-05-16 12:18:44 +0200

    added ability to toot media tweets

--- stoerungsmelder.py
+++ stoerungsmelder.py
@@ -9,30 +9,52 @@ import os
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 mastodon = Mastodon(
-    access_token = 'stoerungsmelder.secret',   # File for "Your access token", can be created unter "Settings" -> "Development" -> "Your applications"
-    api_base_url = 'https://botsin.space'   # URL that the bot is hosted on
+    # File for "Your access token", can be created unter "Settings" -> "Development" -> "Your applications"
+    access_token='stoerungsmelder.secret',
+    api_base_url='https://botsin.space'   # URL that the bot is hosted on
 )
 lastTootTime = 0
 
+
 class tweet:
     def time(self):
-        return int(self.tweet.find('span',attrs={'class','_timestamp'})['data-time'])
+        return int(self.tweet.find('span', attrs={'class', '_timestamp'})['data-time'])
 
     def content(self):
-        return self.tweet.find('p', attrs={'class','tweet-text'}).text
+        out = self.tweet.find('p', attrs={'class', 'tweet-text'})
+        try:
+            out.a.decompose()
+        except:
+            pass
+        return out.text
+
+    def media(self):
+        media = self.tweet.find_all(
+            'div', attrs={'class', 'AdaptiveMedia-photoContainer'})
+        for i in range(len(media)):
+            media[i] = media[i]['data-image-url']
+        return media
 
     def success(self):
-        return True if type(self.tweet) == type(BeautifulSoup('<b/>',features="html.parser").b) else False
+        return True if type(self.tweet) == type(BeautifulSoup('<b/>', features="html.parser").b) else False
 
     def __init__(self, body):
         self.tweet = body
 
+
 def tweetTooter(thisTweet):
     global lastTootTime
-    nextTweet = tweet(thisTweet.tweet.find_next('div', attrs={'class':'tweet'}))
     if thisTweet.success() and lastTootTime < thisTweet.time():
-        tweetTooter(nextTweet)
-        mastodon.status_post(thisTweet.content())
+        tweetTooter(tweet(thisTweet.tweet.find_next(
+            'div', attrs={'class': 'tweet'})))
+        if thisTweet.media():
+            mediaPost = []
+            for i in range(len(thisTweet.media())):
+                mediaPost.append(mastodon.media_post(urllib.request.urlopen(
+                    thisTweet.media()[i]).read(), "image/jpeg"))
+                mastodon.status_post(thisTweet.content(), media_ids=mediaPost)
+        else:
+            mastodon.status_post(thisTweet.content())
         lastTootTime = thisTweet.time()
     else:
         return
@@ -42,12 +64,13 @@ if __name__ == "__main__":
     URL = 'https://twitter.com/stoerungsmelder'
     twitter = urllib.request.urlopen(URL).read().decode('utf-8').rstrip()
     parsed_html = BeautifulSoup(twitter, features="html.parser")
-    firstTweet = tweet(parsed_html.body.find('div', attrs={'class':'tweet'}))
+    firstTweet = tweet(parsed_html.body.find('div', attrs={'class': 'tweet'}))
     try:
         lastTootTime = pickle.load(open("./lastTootTime.log", 'rb'))
     except:
         print("Time-file does not exist. Creating one.")
         lastTootTime = _time.time()
         pickle.dump(lastTootTime, open("./lastTootTime.log", 'wb'))
+
     tweetTooter(firstTweet)
     pickle.dump(lastTootTime, open("./lastTootTime.log", 'wb'))
